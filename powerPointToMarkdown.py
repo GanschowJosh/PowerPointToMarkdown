769582f626b62f7ffae7315b5346c948e59fa2c8
Joshua Ganschow <ganschowjosh@gmail.com>
2025-11-20 13:58:54 -0600

    Refactor conversion logic in convertPPTX2MD function to use pathlib and improve error handling. using markitdown for pdf conversion

--- powerPointToMarkdown.py
+++ powerPointToMarkdown.py
@@ -2,45 +2,56 @@
 takes every .pptx file in a given directory and outputs converted to md in a specified directory
 """
 import os
+from markitdown import MarkItDown
 from pptx2md import convert, ConversionConfig
+from pathlib import Path
 
-def convertPPTX2MD(inputFolder, outputFolder):
-    if not os.path.exists(outputFolder):
-        os.makedirs(outputFolder)
+def convertPPTX2MD(input_folder, output_folder):
+    output_folder.mkdir(parents=True, exist_ok=True)
+    
+    md = MarkItDown()
+    converted = False
 
-    for filename in os.listdir(inputFolder):
+    for path in input_folder.iterdir():
+        if not path.is_file(): continue
+
         try:
-            if filename.endswith(".pptx"):
-                inputPath = os.path.join(inputFolder, filename)
-                outputPath = os.path.join(outputFolder, os.path.splitext(filename)[0] + ".md")
-                curr_conversion_config = ConversionConfig(pptx_path=inputPath, output_path=outputPath, image_dir=f"{outputFolder}/img", disable_notes=True)
-                convert(curr_conversion_config)
+            suffix = path.suffix.lower()
+            output_path = output_folder / path.with_suffix(".md").name
+            if suffix == ".pdf":
+                result = md.convert(path)
+                output_path.write_text(formatting(result.text_content), encoding='utf-8')
+            elif suffix == '.pptx':
+                image_dir = output_folder/'img'
+                image_dir.mkdir(parents=True, exist_ok=True)
+                convert(ConversionConfig(
+                    pptx_path=path,
+                    output_path=output_path,
+                    image_dir=image_dir,
+                ))
+            converted = True
         except Exception as e:
-            print(f"An error ({e}) occurred converting {filename}")
+            print(f"An error ({e}) occurred converting {path.name}")
             continue
-    mergeMdFiles(outputFolder)
+    if converted: mergeMdFiles(output_folder)
 
 def formatting(string):
-    return string.replace(" __", "__").replace("\\-", "-").replace("\\.", ".").replace("\\,", ",").replace("\\(", "(").replace("\\)", ")").replace("\\#", "#").replace("\\+", "+").replace("\\!", "!").replace("\\[", "[").replace("\\]", "]").replace("\\_", "_")
+    return string.replace(" __", "__").replace("\\-", "-").replace("\\.", ".").replace("\\,", ",").replace("\\(", "(").replace("\\)", ")").replace("\\#", "#").replace("\\+", "+").replace("\\!", "!").replace("\\[", "[").replace("\\]", "]").replace("\\_", "_").replace('\x0c', '')
 
 def mergeMdFiles(directory):
-    mergedContent = ""
+    merged_content = ""
     
-    # Iterate over all files in the directory
-    for filename in os.listdir(directory):
-        # Check if the file is a .md file
-        if filename.endswith(".md"):
-            # Open the .md file
-            with open(os.path.join(directory, filename), 'r', encoding='utf-8') as file:
-                # Read the content and add it to the merged content
-                mergedContent += formatting(file.read()) + "\n\n"
-                
-    # Write the merged content to a new .md file
-    with open(os.path.join(directory, "Merged.md"), 'w', encoding='utf-8') as file:
-        file.write(mergedContent)
+    for path in directory.glob("*.md"):
+        if path.name == "Merged.md": continue
+
+        text = path.read_text(encoding='utf-8')
+        merged_content += formatting(text) + "\n\n"
+    
+    merged_path = directory / "Merged.md"
+    merged_path.write_text(merged_content, encoding='utf-8')
 
 if __name__ == "__main__":
-    inputFolder = input("Input folder:\t").replace("\\", "/") 
-    outputFolder = input("Output folder:\t").replace("\\", "/")
+    inputFolder = Path(input("Input folder:\t").strip())
+    outputFolder = Path(input("Output folder:\t").strip())
 
     convertPPTX2MD(inputFolder, outputFolder)
